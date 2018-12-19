f0d704a87227a1d59d7d6158dc448b3a9bc66f6c
BaturayEminoglu <baturay.emin@gmail.com>
2018-12-20 00:22:59 +0300

    Pong project created.

---
+++ Assets/Ball.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody>().velocity = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity.normalized;
+	}
+
+	void OnCollisionEnter(Collision collision) 
+
+	{
+		if (collision.collider.name == "BorderLeft") 
+		{
+		} 
+		else if (collision.collider.name == "BorderRight") 
+		{
+		} 
+		else if (collision.collider.name == "BorderTop") 
+		{
+			GetComponent<Rigidbody>().velocity = new Vector3 (GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z * -1.0f);
+		} 
+		else if (collision.collider.name == "BorderBottom") 
+		{
+			GetComponent<Rigidbody>().velocity = new Vector3 (GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z * -1.0f);
+		} 
+		else if (collision.collider.name == "RacketLeft") 
+		{
+			GetComponent<Rigidbody>().velocity = new Vector3 (GetComponent<Rigidbody>().velocity.z, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.x * -1.0f);
+		}
+		else if (collision.collider.name == "RacketRight")  
+		{
+			GetComponent<Rigidbody>().velocity = new Vector3 (GetComponent<Rigidbody>().velocity.z, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.x * -1.0f);
+		}
+	}
+	}
+		
+
+
---
+++ Assets/Rackets.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rackets : MonoBehaviour {
+	public KeyCode key_up;   
+	public KeyCode key_down;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate() {
+
+		Vector3 pos = transform.position;
+
+		if (Input.GetKey(key_up)) 
+		{
+			transform.position = new Vector3(pos.x,pos.y,pos.z + 0.1f);
+		} 
+		else if (Input.GetKey(key_down)) 
+		{
+			transform.position = new Vector3 (pos.x, pos.y, pos.z - 0.1f);
+		}
+	}
+}
